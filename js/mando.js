61b1e030636e26d057256cf80c5ffd470f4f3d83
cristian gomez <cristiangomez@MacBook-Pro-de-cristian.local>
2020-09-25 14:55:09 -0500

    prueba de git php

--- js/mando.js
+++
@@ -1,5 +0,0 @@
-var mando = {
-    objeto: null,
-    eventosDisponible: 'ongamepadconnected' in window,
-
-}
---
+++ view/datos.php
@@ -0,0 +1,8 @@
+<html>
+    <title>
+        ingresa datos
+    </title>
+    <body>
+        <h1>datos</h1>
+    </body>
+</html>
