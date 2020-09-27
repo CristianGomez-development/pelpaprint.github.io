c43702a75879c335563d84551880527694e634df
cristian gomez <cristiangomez@MacBook-Pro-de-cristian.local>
2020-09-27 08:59:48 -0500

    prueba de git php

--- php/archivo.php
+++ php/archivo.php
@@ -1,16 +1,10 @@
 <?php
-print_r($_FILES);
 $nombre = $_FILES['userfile']['name'];
 $guardado = $_FILES['userfile']['tmp_name'];
-var_dump($nombre);
-var_dump($guardado);
 
 if(!file_exists('archivos')){
     mkdir('../archivos', 0777, true);
     if(file_exists('../archivos')){
-        echo "paso el if existe";
-        var_dump($nombre);
-        var_dump($guardado);
         if(move_uploaded_file($guardado, '../archivos/'.$nombre)){
             echo "Archivo guardado con exito!!!";
         }else{
