a535896c2109c18b37707fc88975873504584ffe
cristian gomez <cristiangomez@MacBook-Pro-de-cristian.local>
2020-09-27 09:44:13 -0500

    guardar archivo servidor

--- php/archivo.php
+++ php/archivo.php
@@ -1,5 +1,4 @@
 <?php
-print_r($_FILES);
 $nombre = $_FILES['userfile']['name'];
 $guardado = $_FILES['userfile']['tmp_name'];
 
@@ -7,14 +6,15 @@ if(!file_exists('archivos')){
     mkdir('../archivos', 0777, true);
     if(file_exists('../archivos')){
         if(move_uploaded_file($guardado, '../archivos/'.$nombre)){
-            echo "Archivo guardado con exito!!!";
+            header("Location:../view/endbuy.html");
         }else{
             echo "El archivo no se guardado";
         }
     }
 }else{
     if(move_uploaded_file($guardado, '../archivos/'.$nombre)){
-        echo "Archivo guardado con exito!!!";
+//        echo "Archivo guardado con exito!!!";
+        header("Location:../view/endbuy.html");
     }else{
         echo "El archivo no se guardado";
     }
